6cbab43c123cad1665ad89e6bed14ebeb010aa06
Mohammed Naser <mnaser@vexxhost.com>
2017-01-17 14:39:08 -0500

    refactored to use setup_config.py

--- run_tests.py
+++ run_tests.py
@@ -1,37 +1,13 @@
 #!/usr/bin/env python
 
-import errno
 import os
 import sys
-import yaml
 
-# If we're not running in CI, fail.
-if os.getenv('CONTINUOUS_INTEGRATION') != 'true':
-    sys.exit(errno.EINVAL)
-
-# Load configuration file
-with open('molecule.yml', 'r') as fd:
-    config = yaml.load(fd)
-
-# Append the build number
-for i in config['openstack']['instances']:
-    i['name'] = "%s-%s" % (i['name'], os.getenv('TRAVIS_BUILD_NUMBER'))
-
-# Change molecule_dir so that it uses different keys
-# https://github.com/metacloud/molecule/blob/master/molecule/driver/openstackdriver.py#L282-L285
-mc = config.get('molecule', {})
-mc['molecule_dir'] = ".molecule-%s" % os.getenv('TRAVIS_BUILD_NUMBER')
-config['molecule'] = mc
-
-# Create updated configuration file
-data = yaml.dump(config)
-with open('molecule.yml', 'w') as fd:
-    fd.write(data)
-
-# Print the configuration file
-print data
+# Get path for current file
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Run the tests
+os.system("%s/setup_config.py" % dir_path)
 ret = os.system("molecule test --destroy=always")
 exit_code = os.WEXITSTATUS(ret)
 sys.exit(exit_code)
---
+++ setup_config.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import errno
+import os
+import sys
+import yaml
+
+# If we're not running in CI, fail.
+if not os.getenv('CONTINUOUS_INTEGRATION') and not os.getenv('JENKINS_URL'):
+    sys.exit(errno.EINVAL)
+
+# Select build number
+build = os.getenv('BUILD_NUMBER')
+if not build:
+    build = os.getenv('TRAVIS_BUILD_NUMBER')
+
+# Load configuration file
+with open('molecule.yml', 'r') as fd:
+    config = yaml.load(fd)
+
+# Append the build number
+for i in config['openstack']['instances']:
+    i['name'] = "%s-%s" % (i['name'], build)
+
+# Change molecule_dir so that it uses different keys
+# https://github.com/metacloud/molecule/blob/master/molecule/driver/openstackdriver.py#L282-L285
+mc = config.get('molecule', {})
+mc['molecule_dir'] = ".molecule-%s" % build
+config['molecule'] = mc
+
+# Create updated configuration file
+data = yaml.dump(config)
+with open('molecule.yml', 'w') as fd:
+    fd.write(data)
+
+# Print the configuration file
+print data
